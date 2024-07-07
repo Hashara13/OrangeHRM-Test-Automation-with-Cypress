b4295a160806eee8f2c9d2783c8eecd81c4b95c1
Hashara13 <hnethmin13@gmail.com>
2024-07-07 23:55:11 +0530

    pim test file updated

--- cypress/e2e/Tests/PIM.cy.js
+++ cypress/e2e/Tests/PIM.cy.js
@@ -3,3 +3,24 @@ import { LoginPage } from "../Pages/Login_Page";
 import { PersonalDetails } from "../Pages/PersonalDetailsPage";
 const loginPage = new LoginPage();
 const PersonalInfo=new PersonalDetails();
+beforeEach(function () {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+  });
+  describe("Login & PIM", function () {
+    it("Login_test_01", function () {
+      loginPage.InputUsername("Admin");
+      loginPage.InputPassword("admin123");
+      loginPage.PressLoginButton();
+      
+      PersonalInfo.NavigatePIM();
+      PersonalInfo.InputFullName("Hashara", "Nethmin", "Perera");
+      PersonalInfo.InputiD("855655", "445485");
+      PersonalInfo.InputLicense("555246");
+      PersonalInfo.addStatus();
+      PersonalInfo. selectGender();
+      
+
+    });
+})
